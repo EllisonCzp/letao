dbd6e66c638900b8a1ca12e813e6fb4a58082128
EllisonCzp <2283487595@qq.com>
2019-06-25 02:19:56 +0800

    首页 商品分类页 搜索页 商品列表页

---
+++ public/m/js/search.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.xt_search a').on('tap', function() {
+        // 跳转去搜索列表页 并且带上关键字
+        var key = $.trim($('input').val());
+        // 判断是否有关键字 无提示用户输入关键字
+        if (!key) {
+            // mui消息提示
+            mui.toast('请输入搜索内容');
+            return false;
+        }
+        // 合法就跳转
+        location.href = './searchList.html?key' + key;
+    });
+})
--- public/m/js/searchList.js
+++ public/m/js/searchList.js
@@ -4,10 +4,4 @@ $(function() {
         deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
         indicators: false
     });
-
-    // 轮播图
-    //获得slider插件对象
-    mui('.mui-slider').slider({
-        interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
-    });
-})
+});
